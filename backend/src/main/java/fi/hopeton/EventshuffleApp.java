0d1c40eb4ee719db27679147225788608c21bbb8
se_henu <htoivakka@gmail.com>
2022-02-08 17:27:13 +0200

    Configure maven and add OpenAPI module to the project for interface segregation from the i

---
+++ backend/src/main/java/fi/hopeton/EventshuffleApp.java
@@ -0,0 +1,13 @@
+package fi.hopeton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventshuffleApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventshuffleApp.class, args);
+    }
+
+}
